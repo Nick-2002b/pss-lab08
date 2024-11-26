dd242de263556e37821c3db86e166761631b0072
Nick_2002b <nicolae.balaban@studio.unibo.it>
2024-11-26 11:04:37 +0100

    Implement the program following the comments

--- 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,8 @@
 package it.unibo.collections.sets;
 
 import java.util.Collection;
+import java.util.TreeSet;
+import java.util.Set;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +26,45 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+        final Set<String> tree = new TreeSet<>();
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        for (int i = 1; i <= ELEMS; i++) {
+            tree.add(Integer.toString(i));
+        }
         /*
          * 3) Prints its content
          */
+        System.out.println("Initial Content of TreeSet: " + tree);
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+        final var iter = tree.iterator();
+        while(iter.hasNext()) {
+            if (Integer.parseInt(iter.next()) % 3 == 0) {
+                iter.remove();
+            }
+        }
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        System.out.println("Contenuto dopo rimozione dei divisibili per 3:");
+        for (String s : tree) {
+            System.out.println(s);
+        }
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        boolean allEven = true;
+        for(String s : tree) {
+            if(Integer.parseInt(s) % 2 != 0) {
+                allEven = false;
+                break;
+            }
+        }
+        System.out.println("Tutti i numeri rimanenti sono pari: " + allEven);
     }
 }
